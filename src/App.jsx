ad63f084bed87d7eb72ee682545103cd7cecba58
hela abada <abadahela@gmail.com>
2023-02-28 00:51:02 +0100

    react-ga4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import {useState } from "react"
+import React, {useEffect} from 'react'
 import Experience from "./Components/Container/Experience";
 import { Navbar, Home, About, Portfolio, Testimonials, Contact, Footer } from "./Components/index"
+import ReactGA from "react-ga4";
 
 function App() {
-  
-  const [darkMode, setDarkMode] = useState(false);
-  
+    const TRAKING_ID = "G-EKTSSLN0XZ"
+    ReactGA.initialize(TRAKING_ID);
+    useEffect(() => {
+      ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
+    }, []);
+
+
   return (
     <div className="font-Poppins bg-Solitude dark:bg-slate-900">
      <Navbar/>
